65d2ae6baca712add5ce26c9260cb775c8278579
MoYingJi <moyingjiaw@outlook.com>
2026-09-11 23:47:14 +0800

    build(arch): 把 options 移到 PKGBUILD 内部

--- package/archlinux/workflow.sh
+++ package/archlinux/workflow.sh
@@ -12,24 +12,6 @@ sudo="sudo --set-home --user builder"
 
 pacman -Syu --noconfirm --color=always git
 
-# 查找 /etc/makepkg.conf 并去除 debug 选项
-# https://github.com/Neboer/archlinuxus/blob/main/prepare-arch-env/prepare-makepkg.sh
-(
-    set +H
-    file="/etc/makepkg.conf"
-    eval "$(grep '^OPTIONS=' "$file")"
-    has_debug=0
-    has_negated_debug=0
-    for i in "${!OPTIONS[@]}"; do case "${OPTIONS[i]}" in
-        "debug") OPTIONS[i]="!debug"; has_debug=1;;
-        "!debug") has_negated_debug=1;;
-    esac done
-    [[ $has_debug -eq 0 && $has_negated_debug -eq 0 ]] && OPTIONS+=("!debug")
-    printf -v options_str '%q ' "${OPTIONS[@]}"
-    new_line="OPTIONS=(${options_str})"
-    sed -i.bak "/^OPTIONS=/c$new_line" "$file"
-)
-
 PACKAGER=${PACKAGER:-"MoYingJi <moyingjiaw@outlook.com>"}
 
 echo ::endgroup::
